40318a9b5d7d0b9cecbc95b070e6409c24a477d3
WolfgangDaniel <42138601+WolfgangDaniel@users.noreply.github.com>
2023-09-10 12:00:49 +0200

    renaming

--- src/views/BotOverview.vue
+++ src/views/BotOverview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-sky-700 text-center text-slate-100 py-12">
-    <h1 class="text-4xl font-bold">Conceptual RPA Bot Modeler</h1>
+    <h1 class="text-4xl font-bold">ULOGEN</h1>
   </div>
   <o-collapse
     :open="false"
